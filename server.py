d270a3d600daea269cf82e72b79518d5bb24f541
Samuel Carvalho Locatelli <samuel.locatelli@apisul.com.br>
2017-11-17 15:15:55 -0200

    Server run

--- server.py
+++ server.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from app.index.controller import root_page
+import os
 
 app = Flask(__name__)
 
 app.register_blueprint(root_page)
 
-app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port, debug=True)
